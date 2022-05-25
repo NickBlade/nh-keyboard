69a7a7e88e5828035c908892c8bf3739a0379eef
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2022-05-26 01:56:26 +0200

    chore(all): project re-structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,17 @@
 fx_version "cerulean"
 game "common"
 
-name "NUI Text Input"
+name 'NUI Text Input'
 description "A modular, clean keyboard input for RedM & FiveM"
-author "NeroHiro"
+author 'NickBlade#5623'
 
-ui_page "./UI/index.html"
+ui_page "web/index.html"
 
 files{
-    "./UI/index.html",
-    "./UI/main.css",
-    "./UI/main.js",
+    'web/index.html',
+    'web/assets/main.css',
+    'web/assets/main.js',
 }
 
-client_script "client.lua"
+client_script "client/client.lua"
 
